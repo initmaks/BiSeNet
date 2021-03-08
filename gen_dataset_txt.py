89768b091c56f2ebb2874c4199313c9358a8c285
Maks <mksmsrkn@gmail.com>
2021-03-08 15:34:16 -0500

    rm extra slash

--- gen_dataset_txt.py
+++ gen_dataset_txt.py
@@ -30,7 +30,7 @@ def main():
                 else:
                     ranges = range(start,stop+1,10)
                 for n in ranges:
-                    color_fname = f"{dataset_path}/{track_param_str}{n:05d}.jpg"
+                    color_fname = f"{dataset_path}{track_param_str}{n:05d}.jpg"
                     label_fname = color_fname.replace("img","cls")
                     label_fname = label_fname.replace("jpg","png")
                     if os.path.isfile(color_fname) and os.path.isfile(label_fname):
