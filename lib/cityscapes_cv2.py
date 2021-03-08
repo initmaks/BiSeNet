ff04a71e7288226bebc22e232bcdaaad13556cde
Maks <mksmsrkn@gmail.com>
2021-03-08 16:11:34 -0500

    cleanup

--- lib/cityscapes_cv2.py
+++ lib/cityscapes_cv2.py
@@ -2,6 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 import os
+import copy
 import os.path as osp
 import json
 
@@ -17,41 +18,41 @@ from lib.base_dataset import BaseDataset, TransformationTrain, TransformationVal
 
 
 labels_info = [
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "unlabeled", "ignoreInEval": True, "id": 0, "color": [0, 0, 0], "trainId": 255},
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "ego vehicle", "ignoreInEval": True, "id": 1, "color": [0, 0, 0], "trainId": 255},
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "rectification border", "ignoreInEval": True, "id": 2, "color": [0, 0, 0], "trainId": 255},
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "out of roi", "ignoreInEval": True, "id": 3, "color": [0, 0, 0], "trainId": 255},
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "static", "ignoreInEval": True, "id": 4, "color": [0, 0, 0], "trainId": 255},
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "dynamic", "ignoreInEval": True, "id": 5, "color": [111, 74, 0], "trainId": 255},
-    {"hasInstances": False, "category": "void", "catid": 0, "name": "ground", "ignoreInEval": True, "id": 6, "color": [81, 0, 81], "trainId": 255},
-    {"hasInstances": False, "category": "flat", "catid": 1, "name": "road", "ignoreInEval": False, "id": 7, "color": [128, 64, 128], "trainId": 0},
-    {"hasInstances": False, "category": "flat", "catid": 1, "name": "sidewalk", "ignoreInEval": False, "id": 8, "color": [244, 35, 232], "trainId": 1},
-    {"hasInstances": False, "category": "flat", "catid": 1, "name": "parking", "ignoreInEval": True, "id": 9, "color": [250, 170, 160], "trainId": 255},
-    {"hasInstances": False, "category": "flat", "catid": 1, "name": "rail track", "ignoreInEval": True, "id": 10, "color": [230, 150, 140], "trainId": 255},
-    {"hasInstances": False, "category": "construction", "catid": 2, "name": "building", "ignoreInEval": False, "id": 11, "color": [70, 70, 70], "trainId": 2},
-    {"hasInstances": False, "category": "construction", "catid": 2, "name": "wall", "ignoreInEval": False, "id": 12, "color": [102, 102, 156], "trainId": 3},
-    {"hasInstances": False, "category": "construction", "catid": 2, "name": "fence", "ignoreInEval": False, "id": 13, "color": [190, 153, 153], "trainId": 4},
-    {"hasInstances": False, "category": "construction", "catid": 2, "name": "guard rail", "ignoreInEval": True, "id": 14, "color": [180, 165, 180], "trainId": 255},
-    {"hasInstances": False, "category": "construction", "catid": 2, "name": "bridge", "ignoreInEval": True, "id": 15, "color": [150, 100, 100], "trainId": 255},
-    {"hasInstances": False, "category": "construction", "catid": 2, "name": "tunnel", "ignoreInEval": True, "id": 16, "color": [150, 120, 90], "trainId": 255},
-    {"hasInstances": False, "category": "object", "catid": 3, "name": "pole", "ignoreInEval": False, "id": 17, "color": [153, 153, 153], "trainId": 5},
-    {"hasInstances": False, "category": "object", "catid": 3, "name": "polegroup", "ignoreInEval": True, "id": 18, "color": [153, 153, 153], "trainId": 255},
-    {"hasInstances": False, "category": "object", "catid": 3, "name": "traffic light", "ignoreInEval": False, "id": 19, "color": [250, 170, 30], "trainId": 6},
-    {"hasInstances": False, "category": "object", "catid": 3, "name": "traffic sign", "ignoreInEval": False, "id": 20, "color": [220, 220, 0], "trainId": 7},
-    {"hasInstances": False, "category": "nature", "catid": 4, "name": "vegetation", "ignoreInEval": False, "id": 21, "color": [107, 142, 35], "trainId": 8},
-    {"hasInstances": False, "category": "nature", "catid": 4, "name": "terrain", "ignoreInEval": False, "id": 22, "color": [152, 251, 152], "trainId": 9},
-    {"hasInstances": False, "category": "sky", "catid": 5, "name": "sky", "ignoreInEval": False, "id": 23, "color": [70, 130, 180], "trainId": 10},
-    {"hasInstances": True, "category": "human", "catid": 6, "name": "person", "ignoreInEval": False, "id": 24, "color": [220, 20, 60], "trainId": 11},
-    {"hasInstances": True, "category": "human", "catid": 6, "name": "rider", "ignoreInEval": False, "id": 25, "color": [255, 0, 0], "trainId": 12},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "car", "ignoreInEval": False, "id": 26, "color": [0, 0, 142], "trainId": 13},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "truck", "ignoreInEval": False, "id": 27, "color": [0, 0, 70], "trainId": 14},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "bus", "ignoreInEval": False, "id": 28, "color": [0, 60, 100], "trainId": 15},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "caravan", "ignoreInEval": True, "id": 29, "color": [0, 0, 90], "trainId": 255},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "trailer", "ignoreInEval": True, "id": 30, "color": [0, 0, 110], "trainId": 255},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "train", "ignoreInEval": False, "id": 31, "color": [0, 80, 100], "trainId": 16},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "motorcycle", "ignoreInEval": False, "id": 32, "color": [0, 0, 230], "trainId": 17},
-    {"hasInstances": True, "category": "vehicle", "catid": 7, "name": "bicycle", "ignoreInEval": False, "id": 33, "color": [119, 11, 32], "trainId": 18},
-    {"hasInstances": False, "category": "vehicle", "catid": 7, "name": "license plate", "ignoreInEval": True, "id": -1, "color": [0, 0, 142], "trainId": -1}
+    {"name": "unlabeled",               "id": 0, "color": [0, 0, 0], "trainId": 255},
+    {"name": "ego vehicle",             "id": 1, "color": [0, 0, 0], "trainId": 255},
+    {"name": "rectification border",    "id": 2, "color": [0, 0, 0], "trainId": 255},
+    {"name": "out of roi",              "id": 3, "color": [0, 0, 0], "trainId": 255},
+    {"name": "static",                  "id": 4, "color": [0, 0, 0], "trainId": 255},
+    {"name": "dynamic",                 "id": 5, "color": [111, 74, 0], "trainId": 255},
+    {"name": "ground",                  "id": 6, "color": [81, 0, 81], "trainId": 255},
+    {"name": "road",                    "id": 7, "color": [128, 64, 128], "trainId": 0},
+    {"name": "sidewalk",                "id": 8, "color": [244, 35, 232], "trainId": 1},
+    {"name": "parking",                 "id": 9, "color": [250, 170, 160], "trainId": 255},
+    {"name": "rail track",              "id": 10, "color": [230, 150, 140], "trainId": 255},
+    {"name": "building",                "id": 11, "color": [70, 70, 70], "trainId": 2},
+    {"name": "wall",                    "id": 12, "color": [102, 102, 156], "trainId": 3},
+    {"name": "fence",                   "id": 13, "color": [190, 153, 153], "trainId": 4},
+    {"name": "guard rail",              "id": 14, "color": [180, 165, 180], "trainId": 255},
+    {"name": "bridge",                  "id": 15, "color": [150, 100, 100], "trainId": 255},
+    {"name": "tunnel",                  "id": 16, "color": [150, 120, 90], "trainId": 255},
+    {"name": "pole",                    "id": 17, "color": [153, 153, 153], "trainId": 5},
+    {"name": "polegroup",               "id": 18, "color": [153, 153, 153], "trainId": 255},
+    {"name": "traffic light",           "id": 19, "color": [250, 170, 30], "trainId": 6},
+    {"name": "traffic sign",            "id": 20, "color": [220, 220, 0], "trainId": 7},
+    {"name": "vegetation",              "id": 21, "color": [107, 142, 35], "trainId": 8},
+    {"name": "terrain",                 "id": 22, "color": [152, 251, 152], "trainId": 9},
+    {"name": "sky",                     "id": 23, "color": [70, 130, 180], "trainId": 10},
+    {"name": "person",                  "id": 24, "color": [220, 20, 60], "trainId": 11},
+    {"name": "rider",                   "id": 25, "color": [255, 0, 0], "trainId": 12},
+    {"name": "car",                     "id": 26, "color": [0, 0, 142], "trainId": 13},
+    {"name": "truck",                   "id": 27, "color": [0, 0, 70], "trainId": 14},
+    {"name": "bus",                     "id": 28, "color": [0, 60, 100], "trainId": 15},
+    {"name": "caravan",                 "id": 29, "color": [0, 0, 90], "trainId": 255},
+    {"name": "trailer",                 "id": 30, "color": [0, 0, 110], "trainId": 255},
+    {"name": "train",                   "id": 31, "color": [0, 80, 100], "trainId": 16},
+    {"name": "motorcycle",              "id": 32, "color": [0, 0, 230], "trainId": 17},
+    {"name": "bicycle",                 "id": 33, "color": [119, 11, 32], "trainId": 18},
+    {"name": "license plate",           "id": -1, "color": [0, 0, 142], "trainId": -1}
 ]
 
 
@@ -65,7 +66,7 @@ class CityScapes(BaseDataset):
         self.n_cats = 19
         self.lb_ignore = 255
         self.lb_map = np.arange(256).astype(np.uint8)
-        for el in labels_info:
+        for el in labels_info: 
             self.lb_map[el['id']] = el['trainId']
 
         self.to_tensor = T.ToTensor(
